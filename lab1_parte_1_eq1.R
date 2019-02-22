f96c8cf31d56a639aaf9d33f342686b205952fc1
Eduardo Ortega <eduor96@hotmail.com>
2019-02-22 13:31:11 -0600

    Quandl.api
    Se agregó el key de Quandl para descargar precios

--- lab1_parte_1_eq1.R
+++ lab1_parte_1_eq1.R
@@ -3,3 +3,6 @@ suppressMessages(library(Quandl)) # Descargar Precios
 install.packages("data.table")
 library(data.table)
 
+options(knitr.table.format = "html") 
+
+Quandl.api_key("PU3GsQWU2zsn6N8_eq71")
